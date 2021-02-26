5d06a3cd8be567b54c7eaa712550372e17888a41
VuThang <65003593+vqthang3791@users.noreply.github.com>
2021-02-26 09:09:30 +0700

    Chapter 7 8

---
+++ Lavarel/Chapter 07/web.php
@@ -0,0 +1,127 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+//    return 'Vũ Quang Thắng';
+    return view('welcome');
+});
+
+Route::get('home','HomeController@showWelcome');
+Route::get('about','AboutController@showDetails');
+
+Route::get('profile/{name}','ProfileController@showProfile');
+/*Route::get('about',function (){
+    return 'About Content';
+});*/
+
+/*Route::get('about/directions',function (){
+    return 'Directions go here';
+});*/
+Route::get('about/directions',array('as' => 'directions', function(){
+    $theURL = URL::route('directions');
+    return "Directions go to this URL: $theURL";
+}));
+
+
+Route::any('submit-form',function (){
+    return 'Process Form';
+});
+Route::get('about/{theSubject}','AboutController@showSubject');
+/*Route::get('about/{theSubject}',function ($theSubject){
+    return $theSubject. ' content goes here';
+});*/
+
+Route::get('about/classes/{theSubject}',function ($theSubject){
+    return " Content on $theSubject";
+});
+
+Route::get('about/classes/{theArt}/{thePrice}',function ($theArt, $thePrice){
+    return "The product: $theArt and $thePrice";
+});
+
+Route::get('where',function (){
+    return Redirect::to('about/directions');
+});
+
+Route::get('/insert',function (){
+    DB::insert('insert into posts(title,body,is_admin) values (?,?,?)',['PHP with Laravel','Laravel is the best fremework !',0]);
+    return 'DONE';
+});
+
+Route::get('/read',function (){
+    $result = DB::select('select * from posts where id = ?',[1]);
+    /* return $result;*/
+    foreach ($result as $post){
+        return $post->body;
+    }
+});
+
+Route::get('update',function (){
+    $updated = DB::update('update posts set title = "New Title hihi" where id > ?',[1]);
+    return $updated;
+});
+
+Route::get('delete',function (){
+    $delete = DB::delete('delete from posts where id = ?',[4]);
+    return $delete;
+});
+
+Route::get('readAll',function (){
+    $posts = Post::all();
+    foreach ($posts as $p){
+        /*return $p->title;*/
+        echo $p->title . " <br> " . $p->body ;
+        echo "<br>";
+    }
+});
+
+Route::get('findId',function (){
+    $posts = Post::where('id','>=',1)
+        ->where('title','PHP with Laravel')
+        ->where('body','like','%new%')
+        ->orderBy('id','desc')
+        ->take(10)
+        ->get();
+    foreach ($posts as $p){
+        /*return $p->title;*/
+        echo $p->title . " <br> " . $p->body ;
+        echo "<br>";
+    }
+});
+
+Route::get('insertORM',function (){
+    $p = new Post;
+    $p->title = 'inserted ORM';
+    $p->body = 'INSERTED done done ORM';
+    $p->is_admin = 1;
+    $p->save();
+});
+
+Route::get('updateORM',function (){
+    /*$p = Post::find(2);*/
+    $p = Post::where('id',5)->first();
+    $p->title = 'updated ORM';
+    $p->body = 'updated Ahihi DONE DONE';
+    $p->save();
+});
+
+Route::get('deleteORM',function (){
+    Post::where('id','>=',14)
+        ->delete();
+});
+
+Route::get('destroyORM',function (){
+    Post::destroy([7,11]);
+});
---
+++ Lavarel/Chapter 08/Post.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Providers;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+}
