f4e2b9ebf965a1a325528d51327093cbbd9af232
VuThang <65003593+vqthang3791@users.noreply.github.com>
2021-02-26 08:55:30 +0700

    Chapter 05

---
+++ Lavarel/Chapter 05/profile.blade.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+    <!DOCTYPE html>
+<html>
+<body>
+<head>
+    <title>Profile</title>
+</head>
+<body>
+<h1>Welcome Profile Page</h1>
+<h2>YOUR NAME {{$name}}</h2>
+</body>
+</body>
+</html>
